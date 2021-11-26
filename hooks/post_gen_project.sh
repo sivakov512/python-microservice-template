564170b88cb21fa12030b829ac4ea5719b2d9865
Nikita Sivakov <sivakov512@gmail.com>
2021-11-26 22:00:33 +0300

    Initialize repo and autocommit new project

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,5 +6,13 @@ git init -b master .
 git submodule add {{ cookiecutter.proto_repository }}
 {%- endif %}
 
-echo 'Okay...'
-echo 'Now install python dependencies with "poetry install"'
+git add ./
+git commit -m "Initial commit"
+
+git remote add origin {{ cookiecutter.git_repo_ssh }}
+git fetch
+git branch -u origin/master master
+
+echo -e '\nOkay...'
+echo 'Now look at the README in your new project and follow instructions.'
+echo -e "Don't forget to star template!\nhttps://github.com/sivakov512/python-microservice-template"
